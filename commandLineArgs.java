42e4edb09d0e369089e8d3d9ba928dcf94c85be4
khushi <khushiporia09@gmail.com>
2025-12-30 18:05:26 +0530

    initialize commit

---
+++ commandLineArgs.java
@@ -0,0 +1,9 @@
+public class commandLineArgs {
+    public static void main(String[] args){
+        System.out.println("command line arguments are: ");
+        for(int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+
+        }
+    }
+}
